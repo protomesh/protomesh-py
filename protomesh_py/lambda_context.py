d46b1d5f565af954375103a32d51fda3cdc6d8f4
Victor França Lopes <18534956+vflopes@users.noreply.github.com>
2023-07-30 17:48:29 -0300

    Add GrpcContext missing methods

--- protomesh_py/lambda_context.py
+++ protomesh_py/lambda_context.py
@@ -1,5 +1,5 @@
 from grpc import ServicerContext, StatusCode
-from typing import Dict, List
+from typing import Dict, List, Callable
 from aws_lambda_typing import context as context_, events, responses
 from .lambda_conversion import convert_grpc_to_http_status_code
 
@@ -9,9 +9,12 @@ class GrpcContext(ServicerContext):
     __code: StatusCode = StatusCode.OK
     __details: str = None
     __service_metadata: Dict[str, List[str]] = {}
+    __context: context_.Context = None
+    __callback: List[Callable] = []
 
     def __init__(self, event: events.APIGatewayProxyEventV1, context: context_.Context):
         
+        self.__context = context
         self.__client_metadata = event["headers"].copy()
 
         for key, value in event["multiValueHeaders"].items():
@@ -44,7 +47,46 @@ class GrpcContext(ServicerContext):
     
     def attach_to_response(self, res: responses.APIGatewayProxyResponseV1):
 
+        for callback in self.__callback:
+            callback()
+
         res["statusCode"] = convert_grpc_to_http_status_code(self.__code).value
 
         for key, value in self.__service_metadata.items():
             res["multiValueHeaders"][key] = value
+
+    def peer(self):
+        raise NotImplementedError()
+
+    def peer_identities(self):
+        raise NotImplementedError()
+    
+    def peer_identity_key(self):
+        raise NotImplementedError()
+    
+    def auth_context(self):
+        raise NotImplementedError()
+    
+    def time_remaining(self):
+        return self.__context.get_remaining_time_in_millis() / 1000.0
+    
+    def cancel(self):
+        raise NotImplementedError()
+    
+    def add_callback(self, callback: callable):
+        self.__callback.append(callback)
+        
+    def is_active(self):
+        return True
+
+    def set_compression(self, compression):
+        raise NotImplementedError()
+    
+    def send_initial_metadata(self, initial_metadata):
+        raise NotImplementedError()
+    
+    def abort_with_status(self, status):
+        raise NotImplementedError()
+    
+    def disable_next_message_compression(self):
+        raise NotImplementedError()
